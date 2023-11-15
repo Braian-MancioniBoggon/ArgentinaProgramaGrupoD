3664eac0226c739817eedaee82e3fe75daf0ff8b
Braian-MancioniBoggon <104107377+Braian-MancioniBoggon@users.noreply.github.com>
2023-11-15 01:26:03 -0300

    añadido Chakra UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
 
   return (
-    <div>App</div>
+    <ChakraProvider>
+      <div>App</div>
+    </ChakraProvider>
   )
 }
 
